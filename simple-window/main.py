182a06dbbdeeec03b93f9daf075699b316bb0d45
Jagdish587 <t.jag587@gmail.com>
2020-10-10 14:21:53 +0530

    sample example

---
+++ simple-window/main.py
@@ -0,0 +1,22 @@
+# This Python file uses the following encoding: utf-8
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
+
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("My Sample App")
+        label = QLabel("This is a simple text !")
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+
